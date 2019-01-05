f111b83bc5acff5023724b4b2b874b16eaf87f0f
Joshua Herrera <herrjosh@oregonstate.edu>
2019-01-04 20:39:06 -0800

    added language setter and getters. Began bible translation getter.

--- bible_gen.py
+++ bible_gen.py
@@ -7,12 +7,61 @@ import requests
 from config import credentials
 
 
+def get_langs(data):
+    # uses initial query to store available languages in a dict
+    # checks if a key exists, if so skips that iteration to store
+    # only unique keys and values.
+    languages = {}
+    for i in data:
+        iso639_lang = i['language']['id']
+        lang_name = i['language']['name']
+        if lang_name not in languages:
+            languages[lang_name.lower()] = iso639_lang
+    return languages
+
+
+def display_langs(languages):
+    # displays available languages
+    for i, j in languages.items():
+        print(j, i.capitalize())
+
+
+def set_lang(languages):
+    # queries the user for a language selection and returns it.
+    while True:
+        lang_selection = input("Select a language using the ISO639 code or full language name: ")
+        lang_selection = lang_selection.lower()
+        if len(lang_selection) == 3 and lang_selection in languages.values():
+            return lang_selection
+        elif lang_selection in languages:
+            return languages[lang_selection]
+        else:
+            print("Invalid selection.")
+
+
+def query_bible_vers(url, headers, lang):
+    parameters = {'language': lang}
+    res = requests.get(url, headers=headers, params=parameters)
+    return res.json()['data']
+
+
+def get_bible_vers(data):
+    pass
+
 def bible_gen():
     # Note, this url will return all bible translations available
     url = "https://api.scripture.api.bible/v1/bibles"
     headers = {'api-key': credentials['key']}
     res = requests.get(url, headers=headers)
-    print(res.json())
+    res_data = res.json()['data']
+    languages = get_langs(res_data)
+    display_langs(languages)
+    chosen_lang = set_lang(languages)
+    print(chosen_lang)
+    # parameters = {'language': chosen_lang}
+    # res = requests.get(url, headers=headers, params=parameters)
+    # print(res.json())
+
 
 
 def main():
